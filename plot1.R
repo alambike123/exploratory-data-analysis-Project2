dde7285dba0fb36ebece8e7a7b996e4fb160c77f
alambike123 <dusaovox@gmail.com>
2020-05-02 23:44:34 +0200

    question 01

---
+++ plot1.R
@@ -0,0 +1,42 @@
+#Unzipping and Loading Files
+library("ggplot2")
+library("dplyr")
+library("data.table")
+path <- getwd()
+
+#download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+#              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+
+#unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+glimpse(NEI)
+
+# QUESTION 01
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from
+# all sources for each of the years 1999, 2002, 2005, and 2008.
+
+
+# Prevents histogram from printing in scientific notation
+#NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+
+question1 <- NEI %>%
+  group_by(year) %>% 
+  summarize(em_year = sum(Emissions))
+
+glimpse(question1)
+question1$em_year <- as.integer(question1$em_year)
+
+png("plot1.png", width=480, height=480)
+
+plot(x = question1$year, y =  question1$em_year
+     , type = "o"
+     , xlab = "Years", ylab = "Emissions"
+     , main = "Emissions over the Years")
+
+dev.off()
